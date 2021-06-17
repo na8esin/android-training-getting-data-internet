77fdd240a8783d37c3df9bbae22c5fb8d7a1891b
渡辺孝之 <na8esin.mpst@gmail.com>
2021-06-17 10:37:29 +0900

    revert GridViewItemBinding -> FragmentOverviewBinding

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
@@ -40,9 +40,9 @@ class OverviewFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //val binding = FragmentOverviewBinding.inflate(inflater)
+        val binding = FragmentOverviewBinding.inflate(inflater)
         // 一時的なコード
-        val binding = GridViewItemBinding.inflate(inflater)
+        //val binding = GridViewItemBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.lifecycleOwner = this
