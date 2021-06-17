bbe28cf14ffde46179592331656d165eed2ec948
渡辺孝之 <na8esin.mpst@gmail.com>
2021-06-17 18:03:49 +0900

    OverviewFragment: ListAdapterを紐付ける

--- app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewFragment.kt
@@ -50,6 +50,8 @@ class OverviewFragment : Fragment() {
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
 
+        // photosGridはRecyclerView
+        binding.photosGrid.adapter = PhotoGridAdapter()
         return binding.root
     }
 }
